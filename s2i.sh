27bbb755d017704921df22080f66452877efde8d
rohit <rohit.ralhan@gmail.com>
2020-12-03 20:05:01 -0600

    s2..sh added

---
+++ s2i.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+unset MAVEN_CONFIG
+
+if [ ! -d "${CHE_PROJECTS_ROOT}/oc_client" ]
+then	
+	cd /projects/	
+	mkdir oc_client
+	wget https://github.com/rohitralhan/occlient/raw/main/oc-4.6.3-linux.tar.gz
+	tar -xvf oc-4.6.3-linux.tar.gz -C oc_client
+	rm -rf oc-4.6.3-linux.tar.gz
+	export PATH=$PATH:/projects/oc_client
+fi
+
+export PATH=$PATH:${CHE_PROJECTS_ROOT}/oc_client 
+
+cd ${CHE_PROJECTS_ROOT}/quarkus-time-table 
+
+oc login -u user1 -p user1 --server=https://api.cluster-838f.838f.example.opentlc.com:6443  --insecure-skip-tls-verify=true  > /dev/null 2>&1
+
+./mvnw clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.kubernetes-client.trust-certs=true
